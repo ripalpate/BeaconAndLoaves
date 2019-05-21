f8516370d5315cffd6652c8fdaaf39bc7a7bacd6
Ripal Patel <ripal212007@gmail.com>
2019-05-21 18:18:27 -0500

    WIP

--- BeaconAndLoaves/Data/PropertyRepository.cs
+++ BeaconAndLoaves/Data/PropertyRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeaconAndLoaves.Models;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,44 @@ namespace BeaconAndLoaves.Data
 {
     public class PropertyRepository
     {
+        readonly string _connectionString;
+
+        public PropertyRepository(IOptions<DbConfiguration> dbConfig)
+        {
+            _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public Property AddProperty(
+            int ownerId, 
+            PropertyType type, 
+            string street, 
+            string city, 
+            string state, 
+            string zipcode, 
+            string description,
+            string imageUrl,
+            decimal price)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var insertQuery = @" Insert into property (name, location, fitnessLevel, UserId)
+                                                            Output inserted.*
+                                                            Values(@name, @location, @fitnessLevel, @userId)";
+                var parameters = new
+                {
+                    Name = name,
+                    Location = location,
+                    FitnessLevel = fitnessLevel,
+                    UserId = userId
+                };
+                var newTarget = db.QueryFirstOrDefault<Target>(insertQuery, parameters);
+
+                if (newTarget != null)
+                {
+                    return newTarget;
+                }
+            }
+            throw new Exception("Could not create target");
+        }
     }
 }
