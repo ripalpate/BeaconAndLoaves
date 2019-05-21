c9ef746599e67542e9354b9554296ed58eb10201
Shane D Wilson <me@shanedwilson.com>
2019-05-20 22:49:05 -0500

    user model complete

---
+++ BeaconAndLoaves/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeaconAndLoaves.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirebaseId { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IsOwner { get; set; }
+        public string IsActive { get; set; }
+    }
+}
