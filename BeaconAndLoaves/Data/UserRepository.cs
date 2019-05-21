2fab13a7b10cca065641740a71896570de867a9e
Shane D Wilson <me@shanedwilson.com>
2019-05-21 18:37:59 -0500

    created addUser and getAllUsers methods in user repository

--- BeaconAndLoaves/Data/UserRepository.cs
+++ BeaconAndLoaves/Data/UserRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeaconAndLoaves.Models;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,37 @@ namespace BeaconAndLoaves.Data
 {
     public class UserRepository
     {
+        const string ConnectionString = "Server=localhost;Database=BeaconAndLoaves;Trusted_Connection=True";
+
+        public User AddUser(string email, string firebaseId, string name, string street, string city,
+                            string state, string zipcode, string phoneNumber, bool isOwner)
+        {
+            using(var db = new SqlConnection(ConnectionString))
+            {
+                var newUser = db.QueryFirstOrDefault<User>(@"
+                    insert into users (email, firebaseId, name, street, city, state, zipcode, phoneNumber, isOwner)
+                    output inserted.*
+                    values (@email, @firebaseId, @name, @street, @city, @state, @zipcode, @phoneNumber, @isOwner)",
+                    new { email, firebaseId, name, street, city, state, zipcode, phoneNumber, isOwner});
+
+                if(newUser != null)
+                {
+                    return newUser;
+                }
+            }
+            throw new Exception("No user created");
+        }
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var users = db.Query<User>(@"
+                    select id, email, firebaseId, name, street, city
+                    state, zipcode, phonenumber, isowner from users
+                    where isactive = 1").ToList();
+
+                return users;
+            }
+        }
     }
 }
