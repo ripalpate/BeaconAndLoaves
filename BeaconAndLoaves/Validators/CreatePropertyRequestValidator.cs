3a434199995f252e0c8e7f53f293529d140a2ff9
Ripal Patel <ripal212007@gmail.com>
2019-05-21 17:38:56 -0500

    Property model done

---
+++ BeaconAndLoaves/Validators/CreatePropertyRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using BeaconAndLoaves.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeaconAndLoaves.Validators
+{
+    public class CreatePropertyRequestValidator
+    {
+        public bool Validate(CreatePropertyRequest requestToValidate)
+        {
+            return (string.IsNullOrEmpty(requestToValidate.PropertyName)
+                   || string.IsNullOrEmpty(requestToValidate.Type.ToString())
+                   || string.IsNullOrEmpty(requestToValidate.Street)
+                   || string.IsNullOrEmpty(requestToValidate.City)
+                   || string.IsNullOrEmpty(requestToValidate.State)
+                   || string.IsNullOrEmpty(requestToValidate.ZipCode)
+                   || string.IsNullOrEmpty(requestToValidate.Description)
+                   || string.IsNullOrEmpty(requestToValidate.ImageUrl)
+                   || string.IsNullOrEmpty(requestToValidate.Price.ToString())
+                   );
+        }
+    }
+}
