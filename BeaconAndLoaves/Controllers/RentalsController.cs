fca502fff984833daf885af3942a36e36aef19ee
Ripal Patel <ripal212007@gmail.com>
2019-05-25 09:41:07 -0500

    Rental Update

--- BeaconAndLoaves/Controllers/RentalsController.cs
+++ BeaconAndLoaves/Controllers/RentalsController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BeaconAndLoaves.Data;
+using BeaconAndLoaves.Models;
+using BeaconAndLoaves.Validators;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +14,24 @@ namespace BeaconAndLoaves.Controllers
     [ApiController]
     public class RentalsController : ControllerBase
     {
+        readonly RentalRepository _repo;
+        readonly CreateRentalRequestValidator _validator;
+        public RentalsController(RentalRepository repo)
+        {
+            _repo = repo;
+            _validator = new CreateRentalRequestValidator();
+        }
+
+        //Update Rental
+        [HttpPut("{id}")]
+        public ActionResult UpdateRental(int id, Rental rentalToUpdate)
+        {
+            if (id != rentalToUpdate.Id)
+            {
+                return BadRequest();
+            }
+            var rental = _repo.UpdateProperty(rentalToUpdate);
+            return Ok(rental);
+        }
     }
 }
--- BeaconAndLoaves/Data/RentalRepository.cs
+++ BeaconAndLoaves/Data/RentalRepository.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Options;
+﻿using BeaconAndLoaves.Models;
+using Dapper;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +16,30 @@ namespace BeaconAndLoaves.Data
         public RentalRepository(IOptions<DbConfiguration> dbConfig)
         {
             _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        //update rental
+        public Rental UpdateProperty(Rental rentalToUpdate)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql =
+                    @"Update Rentals 
+                      Set PropertyId= @propertyId,
+	                    UserPaymentId= @userPaymentId,
+	                    StartDate= @startDate, 
+	                    EndDate= @endDate, 
+	                    RentalAmount= @rentalAmount, 
+                      Where Id = @id";
+
+                var rowsAffected = db.Execute(sql, rentalToUpdate);
+
+                if (rowsAffected == 1)
+                {
+                    return rentalToUpdate;
+                }
+                throw new Exception("Could not update rental");
+            }
         }
     }
 }
