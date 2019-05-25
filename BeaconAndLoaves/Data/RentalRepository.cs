4654043dd78edbc512f4396b685c2123138d51ca
Robert Rice <rrice1291@gmail.com>
2019-05-25 09:42:25 -0500

    wip

--- BeaconAndLoaves/Data/RentalRepository.cs
+++ BeaconAndLoaves/Data/RentalRepository.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Dapper;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +16,20 @@ namespace BeaconAndLoaves.Data
         {
             _connectionString = dbConfig.Value.ConnectionString;
         }
+
+        public IEnumerable<RentalRepository> GetAllRentals()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var rentals = db.Query<RentalRepository>(@"
+                    select * 
+                    from rentals
+                    ").ToList();
+
+                return rentals;
+            }
+        }
+
+
     }
 }
