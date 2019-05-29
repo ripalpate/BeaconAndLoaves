dc03f5dbf8d8c3146e60ecd82808fc68f3715206
Shane D Wilson <me@shanedwilson.com>
2019-05-29 02:12:02 -0500

    login w/ google is workin

--- BeaconAndLoaves/ClientApp/src/App.js
+++ BeaconAndLoaves/ClientApp/src/App.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Auth from './components/pages/Auth';
+import MyNavbar from './components/MyNavbar';
+import authRequests from './helpers/data/authRequests';
+import connection from './helpers/data/connection';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component { ...props } {...rest} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 export default class App extends Component {
   displayName = App.name
 
+  state = {
+    authed: false,
+    pendingUser: true,
+    authBg: true,
+  }
+
+  setAuthBg = () => {
+    this.setState({ authBg: false });
+  }
+
+  componentDidMount() {
+    connection();
+
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          pendingUser: false,
+        });
+      } else {
+        this.setState({
+          authed: false,
+          pendingUser: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const {
+      authed,
+      pendingUser,
+      authBg,
+    } = this.state;
+
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false, authBg: true });
+    };
+
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar isAuthed={ authed } logoutClickEvent={logoutClickEvent} />
+                <Switch>
+                  <PublicRoute path='/auth'
+                    component={Auth}
+                    authed={this.state.authed}
+                    authBg={authBg}
+                    setAuthBg={this.setAuthBg}
+                    />
+                </Switch>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
     );
   }
 }
---
+++ BeaconAndLoaves/ClientApp/src/components/pages/Auth.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
+
+class Auth extends React.Component {
+
+    authenticateUser = (e) => {
+      e.preventDefault();
+      authRequests.authenticate()
+        .then(() => {
+          this.props.setAuthBg();
+          this.props.history.push('/beans');
+        })
+        .catch(error => console.error('there was a problem with auth', error));
+    }
+
+    render() {
+        return (
+          <div className="Auth mx-auto">
+            <div className="btn-container mt-5">
+              <button className="bttn-pill bttn-lg bttn-danger" onClick={this.authenticateUser}>
+                <i className="fab fa-google"></i> Sign In w/Google
+              </button>
+            </div>
+          </div>
+        );
+      }
+}
+
+export default Auth;
