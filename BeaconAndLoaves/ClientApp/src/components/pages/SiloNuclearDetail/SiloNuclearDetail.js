cb41ffa96143ebab19c684f9f0539b9cdd2bd1f7
Ripal Patel <ripal212007@gmail.com>
2019-06-01 13:49:52 -0500

    desplaying silo nuclear detail

--- BeaconAndLoaves/ClientApp/src/components/pages/SiloNuclearDetail/SiloNuclearDetail.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/SiloNuclearDetail/SiloNuclearDetail.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import propertiesRequests from '../../../helpers/data/propertiesRequests';
 
 class SiloNuclearDetail extends React.Component {
+  state = {
+    siloNuclear: []
+  }
+
+  componentDidMount() {
+    const siloNuclearId = this.props.match.params.id;
+    propertiesRequests.getProperties()
+      .then((properties) => {
+        const siloNuclears = properties.filter(property => property.type === 1);
+        const siloNuclear= siloNuclears.find(property => property.id == siloNuclearId);
+        this.setState( {siloNuclear});
+      }).catch(err => console.error(err));
+  }
   render() {
+    const{siloNuclear}= this.state;
     return (
-      <div className="detailPage">
-        <h3>Detail Page</h3>
-      </div>
+      <div className="card ml-4 bg-light detail">
+          <div className="imgHolder">
+            <img className="singleLightHouseImg" src={siloNuclear.imageUrl} alt="siloNuclear"/>
+          </div>
+          <div className="card-body">
+            <h5>{siloNuclear.propertyName}</h5>
+            <p>{siloNuclear.street}</p>
+            <p>{siloNuclear.city}, {siloNuclear.state} - {siloNuclear.zipCode}</p>
+            <p>{siloNuclear.description}</p>
+            <p>${siloNuclear.price}/per night</p>
+          </div>
+        </div>
     );
   }
 }
