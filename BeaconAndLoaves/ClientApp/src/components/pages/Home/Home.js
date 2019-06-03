72fb3cb4b8689e7fa69a2a11a441c1511be6b013
Ripal <ripal212007@gmail.com>
2019-06-03 18:22:31 -0500

    modified homepage

--- BeaconAndLoaves/ClientApp/src/components/pages/Home/Home.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/Home/Home.js
@@ -24,6 +24,20 @@ class Home extends React.Component {
               <p className="card-text">Properties To Rent</p>
             </div>
           </div>
+          <div className="card border-dark" id='likedProperties' onClick={this.changeView}>
+            <div className="card-body home text-center">
+              <h4 className="card-title"><i className="fas fa-heart fa-6x home-likedProperties"></i></h4>
+              <h5 className="card-subtitle mb-2 text-muted">Properties</h5>
+              <p className="card-text">Saved Properties that I like most</p>
+            </div>
+          </div>
+          <div className="card border-dark" id='addProperty' onClick={this.changeView}>
+            <div className="card-body home text-center">
+              <h4 className="card-title"><i className="fas fa-plus-circle fa-6x home-addProperty"></i></h4>
+              <h5 className="card-subtitle mb-2 text-muted">Add Property</h5>
+              <p className="card-text">Add your property to rent</p>
+            </div>
+          </div>
         </div>
       </div>
     );
