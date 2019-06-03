19ca503fbdb3820cc13acf8af75ca14f8ad3465b
Ripal <ripal212007@gmail.com>
2019-06-03 18:45:46 -0500

    modified Home page

---
+++ BeaconAndLoaves/ClientApp/src/components/pages/AddEditProperty/AddEditProperty.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddEditProperty extends React.Component {
+    render(){
+        return(
+            <div>
+                <h3>Add your property here</h3>
+            </div>
+        )
+    }
+}
+
+export default AddEditProperty;
--- BeaconAndLoaves/ClientApp/src/components/pages/Home/Home.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.scss';
 
 class Home extends React.Component {
   changeView = (e) => {
