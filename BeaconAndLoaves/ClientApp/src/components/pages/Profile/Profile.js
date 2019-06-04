d87155f5610736934af17e526a88789fdd0c3123
Shane D Wilson <me@shanedwilson.com>
2019-06-03 23:48:39 -0500

    editing of profile is working

--- BeaconAndLoaves/ClientApp/src/components/pages/Profile/Profile.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/Profile/Profile.js
@@ -10,6 +10,34 @@ class Profile extends React.Component {
       userPayments: [],
       properties: [],
     },
+    isEditing: false,
+    userId: 0,
+  }
+
+  formFieldStringState = (name, e) => {
+    const tempUser = { ...this.state.currentUser };
+    tempUser[name] = e.target.value;
+    this.setState({ currentUser: tempUser });
+  }
+
+  emailChange = e => this.formFieldStringState('email', e);
+
+  nameChange = e => this.formFieldStringState('name', e);
+
+  streetChange = e => this.formFieldStringState('street', e);
+
+  cityChange = e => this.formFieldStringState('city', e);
+
+  stateChange = e => this.formFieldStringState('state', e);
+
+  zipCodeChange = e => this.formFieldStringState('zipCode', e);
+
+  phoneNumberChange = e => this.formFieldStringState('phoneNumber', e);
+
+  editProfile = (e) => {
+    const { currentUser } = this.state;
+    this.setState({ isEditing: true });
+    this.setState({ userId: currentUser.id });
   }
 
   changeView = (e) => {
@@ -29,12 +57,175 @@ class Profile extends React.Component {
     this.setState({ accountId: selectedAccount });
   }
 
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { userId, currentUser } = this.state;
+    userRequests.updateUser(userId, currentUser)
+      .then(() => {
+        this.setState({ isEditing: false });
+      });
+  }
+
   componentDidMount() {
     this.getUser();
   }
 
   render() {
-    const { currentUser } = this.state;
+    const {
+      currentUser,
+      isEditing,
+    } = this.state;
+
+    const makeProfileCard = () => {
+      if (isEditing) {
+        return (
+          <div className="reg-container mx-auto">
+              <form className="row form-container border border-dark rounded mt-5 mx-auto" onSubmit={this.formSubmit}>
+                <div className="form col-11 mt-2">
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">Email</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">Email</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="email"
+                        placeholder="bob@xxx.com"
+                        value={currentUser.email}
+                        onChange={this.emailChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="name" className="sr-only">Name</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">Name</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        placeholder="Sumatra Wet Process Gunung Tujuh"
+                        value={currentUser.name}
+                        onChange={this.nameChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">Street</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">Street</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="street"
+                        placeholder="123 Main St."
+                        value={currentUser.street}
+                        onChange={this.streetChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">City</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">City</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="city"
+                        placeholder="Springfield"
+                        value={currentUser.city}
+                        onChange={this.cityChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">State</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">State</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="state"
+                        placeholder="TN"
+                        value={currentUser.state}
+                        onChange={this.stateChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">Zip Code</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">Zip Code</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="zipCode"
+                        placeholder="12345"
+                        value={currentUser.zipCode}
+                        onChange={this.zipCodeChange}
+                        />
+                    </div>
+                  </div>
+                  <div className="col-auto form-lines p-0">
+                    <label htmlFor="link" className="sr-only">Phone Number</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                        <div className="input-group-text">Phone Number</div>
+                        </div>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="phoneNumber"
+                        placeholder="615-333-4444"
+                        value={currentUser.phoneNumber}
+                        onChange={this.phoneNumberChange}
+                        />
+                    </div>
+                  </div>
+                </div>
+              <button type="submit" className="btn user-add-btn btn-success my-auto mx-auto" onClick={this.formSubmit}>
+                  <i className="fas fa-plus-circle" />
+              </button>
+              </form>
+          </div>
+        );
+      }
+      return (
+        <div className="card" id={currentUser.id}>
+          <h3 className="text-center">{currentUser.name}</h3>
+          <div className="ml-1">Email: {currentUser.email}</div>
+          <div className="ml-1">Street: {currentUser.street}</div>
+          <div className="ml-1">City: {currentUser.city}</div>
+          <div className="ml-1">State: {currentUser.state}</div>
+          <div className="ml-1">Zipcode: {currentUser.zipCode}</div>
+          <div className="ml-1">Phone Number: {currentUser.phoneNumber}</div>
+          <div className="ml-1">
+            {makeDropdowns()}
+          </div>
+          <div className="ml-1">
+            {makeHistoryButtons()}
+            <button id='renting' type="button" className="btn renter-history-btn btn-success mx-auto" onClick={this.changeView}>
+              See My Renting History
+            </button>
+            <button id='profile-edit' type="button" className="btn profile-edit-btn btn-warning mx-auto" onClick={this.editProfile}>
+              Edit
+            </button>
+          </div>
+        </div>
+      );
+    };
 
     const makeDropdowns = () => {
       if (currentUser.isOwner === true) {
@@ -71,43 +262,22 @@ class Profile extends React.Component {
           </div>);
     };
 
-    const makeButtons = () => {
+    const makeHistoryButtons = () => {
       if (currentUser.isOwner === true) {
         return (
           <div>
           <button id='renting' type="button" className="btn renter-history-btn btn-success text-center" onClick={this.changeView}>
             See My Renting History
           </button>
-          <button id='rental' type="button" className="btn rental-history-btn btn-success text-center" onClick={this.changeView}>
-            See My Renting History
-          </button>
           </div>
         );
       }
-      return (<div>
-            <button id='renting' type="button" className="btn renter-history-btn btn-success mx-auto" onClick={this.changeView}>
-              See My Renting History
-            </button>
-          </div>);
+      return (<div></div>);
     };
 
     return (
       <div className="profileDiv d-flex mx-auto">
-        <div className="card" id="profile">
-          <h3 className="text-center">{currentUser.name}</h3>
-          <div className="ml-1">Email: {currentUser.email}</div>
-          <div className="ml-1">Street: {currentUser.street}</div>
-          <div className="ml-1">City: {currentUser.city}</div>
-          <div className="ml-1">State: {currentUser.state}</div>
-          <div className="ml-1">Zipcode: {currentUser.zipCode}</div>
-          <div className="ml-1">Phone Number: {currentUser.phoneNumber}</div>
-          <div className="ml-1">
-            {makeDropdowns()}
-          </div>
-          <div className="ml-1">
-            {makeButtons()}
-          </div>
-        </div>
+        {makeProfileCard()}
       </div>
     );
   }
