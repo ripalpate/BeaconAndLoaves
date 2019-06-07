9a27fa140ed3c87e4d7d1d6174c720c316526a58
Shane D Wilson <me@shanedwilson.com>
2019-06-06 21:28:04 -0500

    property info printing to page

--- BeaconAndLoaves/ClientApp/src/components/pages/Rental/Rental.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/Rental/Rental.js
@@ -34,23 +34,33 @@ class Rental extends React.Component {
     }
 
     render() {
+      const { propertyToRent } = this.state;
       return (
-        <div className="text-center rental-div">
-            <div id="start">
-                <label>Start Date </label>
-                <DatePicker
-                    className="ml-3"
-                    selected={this.state.startDate}
-                    onChange={this.handleStartChange}
-                />
-            </div>
-            <div id="end">
-                <label>End Date </label>
-                <DatePicker
-                    className="ml-3"
-                    selected={this.state.endDate}
-                    onChange={this.handleEndChange}
-                />
+        <div className="text-center rental-div mx-auto">
+            <div className="profile-card border border-dark rounded" id={propertyToRent.id}>
+                <h3 className="text-center">{propertyToRent.propertyName}</h3>
+                <div className="ml-1">Street: {propertyToRent.street}</div>
+                <div className="ml-1">City: {propertyToRent.city}</div>
+                <div className="ml-1">State: {propertyToRent.state}</div>
+                <div className="ml-1">Zipcode: {propertyToRent.zipCode}</div>
+                <div className="ml-1">Rate: ${propertyToRent.price}/Day</div>
+                <div id="start">
+                    <label>Start Date </label>
+                    <DatePicker
+                        className="ml-3"
+                        selected={this.state.startDate}
+                        onChange={this.handleStartChange}
+                    />
+                </div>
+                <div id="end">
+                    <label>End Date </label>
+                    <DatePicker
+                        className="ml-3"
+                        selected={this.state.endDate}
+                        onChange={this.handleEndChange}
+                    />
+                </div>
+                <div className="ml-1">Total: $</div>
             </div>
         </div>
       );
