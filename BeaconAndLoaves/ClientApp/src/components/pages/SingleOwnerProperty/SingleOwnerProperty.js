280105ca6dfcc8b893bbfff003778fc217cbfa3a
Ripal <ripal212007@gmail.com>
2019-07-08 21:30:42 -0500

    styled owner properties

--- BeaconAndLoaves/ClientApp/src/components/pages/SingleOwnerProperty/SingleOwnerProperty.js
+++ BeaconAndLoaves/ClientApp/src/components/pages/SingleOwnerProperty/SingleOwnerProperty.js
@@ -73,20 +73,20 @@ class SingleOwnerProperty extends React.Component {
     };
 
     return (
-        <div className="card mx-auto bg-light detail" >
+        <div className="card detail ml-4 mb-3 mt-3 animated zoomIn" id="details" >
             <div id={property.id} data-type={property.type} onClick={this.changePropertyToDetailView}>
             <div className="imgHolder">
                 <img className="singleLightHouseImg"src={property.imageUrl} alt="property"/>
             </div>
-            <div className="card-body property-body">
+            <div className="card-body d-flex flex-column property-body">
                 <h5>{property.propertyName}</h5>
                 <p>{property.street}</p>
                 <p>{property.city}, {property.state} - {property.zipCode}</p>
                 <p>{property.description}</p>
                 <p>${property.price}/per night</p>
+                <div className=" button">{makeLikedPropertyButton()}</div>
             </div>
             </div>
-            {makeLikedPropertyButton()}
         </div>
     );
   }
